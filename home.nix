a54d2f9e5f8d84fe4d88690d85e608bca349cb8b
Raphael <rparodi@student.42.fr>
2024-09-12 15:40:30 +0200

    feat: Adding the auto-configuration of neovim removing: Script directy adding on home.nix

--- home.nix
+++ home.nix
@@ -6,11 +6,11 @@
 #    By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/09/12 11:35:56 by rparodi           #+#    #+#              #
-#    Updated: 2024/09/12 11:38:02 by rparodi          ###   ########.fr        #
+#    Updated: 2024/09/12 15:36:15 by rparodi          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -67,6 +67,19 @@
 		zoxide
 		zsh
 	];
+
+	home.activation.myScript = lib.mkAfter ''
+		echo "Checking neovim installation..."
+		if [ ! -d ~/.local/share/nvim/lazy/lazy.nvim ]; then
+			${pkgs.git}/bin/git clone https://github.com/folke/lazy.nvim.git ~/.local/share/nvim/lazy/lazy.nvim
+			echo "neovim: Adding lazy package manager..."
+		fi
+
+		if [ ! -d "$HOME/.config/nvim" ]; then
+			${pkgs.git}/bin/git clone https://github.com/EniumRaphael/neovimconf.git ~/.config/nvim
+			echo "neovim: Adding configuration for neovim..."
+		fi
+	'';
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
@@ -131,74 +144,6 @@
 			tmuxPlugins.vim-tmux-navigator
 			tmuxPlugins.catppuccin
 		];
-		 extraConfig = ''
-			# Set default terminal
-			set -g default-terminal "screen-256color"
-
-			# Change prefix key to Ctrl-a
-			set -g prefix C-a
-			unbind C-b
-			bind C-a send-prefix
-
-			# Split window bindings
-			unbind %
-			bind \\ split-window -h
-			unbind '"'
-			bind | split-window -v
-
-			# Reload tmux configuration
-			unbind r
-			bind r source-file ~/.tmux.conf \; display "Reloaded!"
-
-			setw -g mode-keys vi
-			bind-key h select-pane -L
-			bind-key j select-pane -D
-			bind-key k select-pane -U
-			bind-key l select-pane -R
-
-			# Pane resizing shortcuts
-			bind -n C-S-Up resize-pane -U 5
-			bind -n C-S-Down resize-pane -D 5
-			bind -n C-S-Left resize-pane -L 5
-			bind -n C-S-Right resize-pane -R 5
-
-			# Pane navigation shortcuts
-			bind -n C-Up select-pane -U
-			bind -n C-Down select-pane -D
-			bind -n C-Left select-pane -L
-			bind -n C-Right select-pane -R
-
-			# Toggle pane zoom
-			bind -r m resize-pane -Z
-
-			# Enable mouse mode
-			set -g mouse on
-
-			# Copy mode key bindings for vim-like behavior
-			bind-key -T copy-mode-vi v send-keys -X begin-selection
-			bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
-
-			# Plugin configuration
-			set -g status-position top
-			set -g @catppuccin_flavour 'mocha'
-
-			set -g @catppuccin_window_right_separator "█"
-			set -g @catppuccin_window_number_position "right"
-			set -g @catppuccin_window_middle_separator " █"
-
-			set -g @catppuccin_window_default_fill "number"
-			set -g @catppuccin_window_current_fill "number"
-			set -g @catppuccin_window_current_text "#{pane_current_path}"
-
-			set -g @catppuccin_status_modules_right "application session date_time"
-			set -g @catppuccin_status_left_separator  "██"
-			set -g @catppuccin_status_right_separator "█ "
-			set -g @catppuccin_status_fill "all"
-
-			set -g @catppuccin_status_connect_separator "yes"
-
-			bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "xclip -selection clipboard -in"
-		'';
 	};
 
 	programs.zsh = {
@@ -367,6 +312,75 @@
 		eval "$(${pkgs.direnv}/bin/direnv hook zsh)"
 		eval "$(${pkgs.starship}/bin/starship init zsh)"
 	'';
+
+	home.file.".tmux.conf".text = ''
+			# Set default terminal
+			set -g default-terminal "screen-256color"
+
+			# Change prefix key to Ctrl-a
+			set -g prefix C-a
+			unbind C-b
+			bind C-a send-prefix
+
+			# Split window bindings
+			unbind %
+			bind \\ split-window -h
+			unbind '"'
+			bind | split-window -v
+
+			# Reload tmux configuration
+			unbind r
+			bind r source-file ~/.tmux.conf \; display "Reloaded!"
+
+			setw -g mode-keys vi
+			bind-key h select-pane -L
+			bind-key j select-pane -D
+			bind-key k select-pane -U
+			bind-key l select-pane -R
+
+			# Pane resizing shortcuts
+			bind -n C-S-Up resize-pane -U 5
+			bind -n C-S-Down resize-pane -D 5
+			bind -n C-S-Left resize-pane -L 5
+			bind -n C-S-Right resize-pane -R 5
+
+			# Pane navigation shortcuts
+			bind -n C-Up select-pane -U
+			bind -n C-Down select-pane -D
+			bind -n C-Left select-pane -L
+			bind -n C-Right select-pane -R
+
+			# Toggle pane zoom
+			bind -r m resize-pane -Z
+
+			# Enable mouse mode
+			set -g mouse on
+
+			# Copy mode key bindings for vim-like behavior
+			bind-key -T copy-mode-vi v send-keys -X begin-selection
+			bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+			# Plugin configuration
+			set -g status-position top
+			set -g @catppuccin_flavour 'mocha'
+
+			set -g @catppuccin_window_right_separator "█"
+			set -g @catppuccin_window_number_position "right"
+			set -g @catppuccin_window_middle_separator " █"
+
+			set -g @catppuccin_window_default_fill "number"
+			set -g @catppuccin_window_current_fill "number"
+			set -g @catppuccin_window_current_text "#{pane_current_path}"
+
+			set -g @catppuccin_status_modules_right "application session date_time"
+			set -g @catppuccin_status_left_separator  "██"
+			set -g @catppuccin_status_right_separator "█ "
+			set -g @catppuccin_status_fill "all"
+
+			set -g @catppuccin_status_connect_separator "yes"
+
+			bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "xclip -selection clipboard -in"
+		'';
 
 	#home.sessionVariables = {
 	#XDG_DATA_DIRS = "${pkgs.vesktop}/share:$XDG_DATA_DIRS";
