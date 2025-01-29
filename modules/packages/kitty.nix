80391ba826ce2be837241e8179b30572f6841325
Raphael <rparodi@student.42.fr>
2025-01-29 11:37:07 +0100

    feat: adding the ligature font

--- modules/packages/kitty.nix
+++ modules/packages/kitty.nix
@@ -10,7 +10,13 @@ in
 		];
 		programs.kitty = {
 			enable = true;
+			font = {
+				name = "FiraCode Nerd Font";
+				package = pkgs.nerd-fonts.fira-code;
+				size = 13;
+			};
 			settings = {
+				disable_ligatures = "never";
 				confirm_os_window_close = 0;
 				enable_audio_bell = false;
 			};
