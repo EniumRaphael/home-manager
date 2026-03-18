6641e5070cfe50a2fef38ce87e0a49d39d7bcfb0
Raphael <rparodi@student.42.fr>
2026-03-18 14:56:26 +0100

    feat(wm/hyprlock): adding the hyprlock

---
+++ modules/window-manager/hyprlock.nix
@@ -0,0 +1,48 @@
+{
+  inputs,
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  cfg = config.window-manager.hyprlock;
+in
+  {
+  config = lib.mkIf cfg {
+    home.packages = with pkgs; [
+      hyprlock
+    ];
+    catppuccin.hyprlock.enable = true;
+    programs.hyprlock = {
+      enable = true;
+      settings = {
+        general = {
+          hide_cursor = true;
+          ignore_empty_input = true;
+        };
+
+        animations = {
+          enabled = true;
+          fade_in = {
+            duration = 300;
+            bezier = "easeOutQuint";
+          };
+          fade_out = {
+            duration = 300;
+            bezier = "easeOutQuint";
+          };
+        };
+
+        background = [
+          {
+            path = "screenshot";
+            blur_passes = 3;
+            blur_size = 8;
+          }
+        ];
+      };
+    };
+  };
+}
