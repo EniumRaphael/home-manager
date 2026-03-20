0c6196efc85b4bd252e80ee137e8bbf50cefcc2a
Raphael <rparodi@student.42.fr>
2026-03-20 23:37:16 +0100

    style(wm/hyprpaper): changing wallpapers

--- modules/window-manager/hyprpaper.nix
+++ modules/window-manager/hyprpaper.nix
@@ -8,33 +8,37 @@
 
 let
   cfg = config.window-manager.hyprpaper;
-  image = pkgs.fetchurl {
-    url = "https://www.baltana.com/files/wallpapers-20/Cyberpunk-Pixel-Art-Wallpaper-HD-49317.jpg";
-    sha256 = "sha256-dv5jHAfqJjvl6an5Xl8lMr7xlpvM5F8OrDMRYy/wOSU=";
+  verticalWallpaper = pkgs.fetchurl {
+    url = "https://github.com/EniumRaphael/dotfiles/blob/master/wallpaper_vertical.png?raw=true";
+    sha256 = "sha256-YuxT0+QQb33pcJ12v3lKQEpRT6Ju/ef9+xpWVAAp5sk=";
   };
-  wallpaperPath = "/home/${config.home.username}/Pictures/wallpaper.png";
+  horizonWallpaper = pkgs.fetchurl {
+    url = "https://github.com/zhichaoh/catppuccin-wallpapers/blob/main/misc/windows-error.jpg?raw=true";
+    sha256 = "sha256-Yr31nKWS1yLkxs6GxYQ6jHwKgJ+b9iqPcZbBY0suGLg=";
+  };
 in
 {
   config = lib.mkIf cfg {
     home.packages = with pkgs; [
       hyprpaper
     ];
-    home.file."Pictures/wallpaper.png".source = image;
+    home.file."Pictures/vertical.png".source = verticalWallpaper;
+    home.file."Pictures/horizontal.png".source = horizonWallpaper;
     services.hyprpaper = {
       enable = true;
       settings = {
         ipc = true;
         splash = false;
 
-        preload = [ wallpaperPath ];
+        preload = [ "/home/${config.home.username}/Pictures/horizontal.png" ];
 
         wallpaper = [
           {
-            path = "/home/${config.home.username}/Pictures/wallpaper.png";
+            path = "/home/${config.home.username}/Pictures/vertical.png";
             monitor = "DP-2";
           }
           {
-            path = "/home/${config.home.username}/Pictures/wallpaper.png";
+            path = "/home/${config.home.username}/Pictures/horizontal.png";
             monitor = "HDMI-A-1";
           }
         ];
