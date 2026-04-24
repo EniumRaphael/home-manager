d39d2e36a27a70cd6abe7a33ba5f09ba312f40b3
Raphael <rparodi@student.42.fr>
2026-04-24 15:21:49 +0200

    feat(wm/hyprland): removing the systemd mode for conflict with uwsm

--- modules/window-manager/hyprland.nix
+++ modules/window-manager/hyprland.nix
@@ -46,7 +46,7 @@ in
     wayland.windowManager.hyprland = {
       enable = true;
       systemd = {
-        enable = true;
+        enable = false;
         variables = [
           "--all"
         ];
