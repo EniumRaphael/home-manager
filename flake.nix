ed4175cafab5ce3b3593775947ab3f2d337e35de
Raphael <rparodi@student.42.fr>
2026-04-26 10:48:18 +0200

    feat(nix/flake): adding the zen-browser for follow pkgs

--- flake.nix
+++ flake.nix
@@ -7,7 +7,13 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
     orca-slicer-flake.url = "github:EniumRaphael/orca-slicer-flake";
     hyprland.url = "github:hyprwm/Hyprland";
     nixvim.url = "github:EniumRaphael/nixvim";
