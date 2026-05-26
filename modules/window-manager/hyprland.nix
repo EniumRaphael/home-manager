9bb571979956396ab90b404553dcae81cbdbe0f3
Raphael <rparodi@student.42.fr>
2026-05-26 18:54:02 +0200

    feat(wm/hyprland): disable the hyprland theme

--- modules/window-manager/hyprland.nix
+++ modules/window-manager/hyprland.nix
@@ -73,7 +73,7 @@ in
         networkmanagerapplet
       ];
     catppuccin = {
-      hyprland.enable = true;
+      hyprland.enable = false;
       cursors = {
         enable = true;
         accent = "dark";
