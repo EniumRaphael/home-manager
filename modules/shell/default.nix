27f706989b5706d03e0bdac001044a2d81066319
Raphael Parodi <raphael@parodi.pro>
2026-08-09 19:12:53 +0200

    feat(shell): split the configuration in mutiple files so added an default.nix

---
+++ modules/shell/default.nix
@@ -0,0 +1,52 @@
+{
+  inputs,
+  config,
+  pkgs,
+  lib,
+  gpgFingerprint ? null,
+  ...
+}:
+
+
+let
+  git = import ./git.nix { inherit config pkgs lib gpgFingerprint; };
+  starship = import ./starship.nix { inherit config pkgs lib; };
+  tmux = import ./tmux.nix { inherit config pkgs lib; };
+  zsh = import ./zsh.nix { inherit config pkgs lib; };
+in
+{
+  imports = [
+    git
+    starship
+    tmux
+    zsh
+  ];
+
+  options.shell = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable the shell configuration.";
+    };
+
+    tools = {
+      git = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable the Git configuration.";
+      };
+
+      starship = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable the Starship configuration.";
+      };
+
+      tmux = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable the Tmux configuration.";
+      };
+    };
+  };
+}
--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -2,223 +2,165 @@
   config,
   pkgs,
   lib,
-  gpgFingerprint ? null,
   ...
 }:
 
 let
-  starship = import ./starship.nix { inherit config pkgs; };
-  tmux = import ./tmux.nix { inherit config pkgs; };
+  cfg = config.shell.enable;
 in
 {
-  imports = [
-    starship
-    tmux
-  ];
+  config = lib.mkIf cfg {
 
-  home = {
-    packages = with pkgs; [
-      cowsay
-      perl
-      direnv
-      eza
-      gnupg
-      pinentry-tty
-      nix-output-monitor
-      lolcat
-      wget
-      zoxide
-      unzip
-    ];
-  };
-
-  programs = {
-    gpg = lib.mkIf (gpgFingerprint != null) {
-      enable = true;
-    };
-    fzf = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-    fastfetch = {
-      enable = true;
-    };
-    zoxide = {
-      enable = true;
-    };
-    direnv = {
-      enable = true;
-      silent = true;
-      nix-direnv = {
-        enable = true;
-      };
-    };
-    bat = {
-      enable = true;
-      config.theme = "Catppuccin Mocha";
-    };
-    git = {
-      enable = true;
-      ignores = [
-        ".DS_Store"
-        "*.swp"
-        "*.swo"
-        "*.swm"
-        "*.swu"
-        "node_modules/"
-        ".env"
+    home = {
+      packages = with pkgs; [
+        cowsay
+        perl
+        direnv
+        eza
+        gnupg
+        pinentry-tty
+        nix-output-monitor
+        lolcat
+        wget
+        zoxide
+        unzip
       ];
-      settings = {
-        alias = {
-          "a" = "add";
-          "c" = "commit";
-          "d" = "diff";
-          "h" = "log --oneline";
-          "m" = "commit -m";
-          "p" = "push";
-          "r" = "restore";
-          "s" = "status";
-          "br" = "branch";
-          "cl" = "clone";
-          "co" = "checkout";
-          "pf" = "push --force-with-lease";
-          "pp" = "pull";
-          "rb" = "rebase";
-          "ss" = "status --short";
-          "rbi" = "rebase -i";
-        };
-        core = {
-          editor = "nvim";
-          pager = "bat -p";
-        };
-        pull = {
-          rebase = true;
-        };
-        push = {
-          autoSetupRemote = true;
-        };
-        user = {
-          name = "Raphael Parodi";
-          email = "raphael@parodi.pro";
-        };
-        signing = lib.mkIf (gpgFingerprint != null) {
-          key = gpgFingerprint;
-          format = "openpgp";
-          signByDefault = true;
-        };
-      };
     };
-    zsh = {
-      enable = true;
-      completionInit = ''
-        autoload -U compinit
-        compinit
-      '';
-      autosuggestion = {
-        enable = true;
-      };
-      history = {
-        append = true;
-        expireDuplicatesFirst = false;
-        extended = false;
-        findNoDups = true;
-        ignoreAllDups = true;
-        ignoreDups = true;
-        ignoreSpace = true;
-        path = "${config.home.homeDirectory}/.zsh_history";
-        save = 5000;
-        share = true;
-        size = 5000;
-      };
-      enableCompletion = true;
-      autocd = true;
-      historySubstringSearch = {
-        enable = true;
-      };
-      shellAliases = {
-        "cls" = "clear";
-        "vim" = "nvim";
-        "nrs" = "sudo nixos-rebuild switch --flake /etc/nixos#$(hostname) |& nom";
-        "hms" = "home-manager switch --flake ~/.config/home-manager#$(hostname) --impure |& nom";
-        "ls" = "eza -h --icons=always";
-        "ll" = "ls -l";
-        "l" = "ls -l";
-        "la" = "ls -la";
-        "vg" =
-          "valgrind --leak-check=full --show-leak-kinds=all --trace-children=yes --track-fds=yes --track-origins=yes --show-reachable=yes";
-        "ssh" = "TERM=xterm-256color ssh";
-      };
-      zplug = {
-        enable = true;
-        zplugHome = "${config.xdg.dataHome}/.zplug";
-        plugins = [
-          { name = "Aloxaf/fzf-tab"; }
-          { name = "nix-community/nix-zsh-completions"; }
-          { name = "z-shell/F-Sy-H"; }
-          { name = "zsh-users/zsh-autosuggestions"; }
-          { name = "zsh-users/zsh-syntax-highlighting"; }
-        ];
-      };
-      envExtra = ''
-        [ -f "$HOME/.zvars"  ] && source "$HOME/.zvars";
-        [ -f "$HOME/.nix-profile/etc/profile.d/nix.sh" ]  && source "$HOME/.nix-profile/etc/profile.d/nix.sh"
-        [ -f "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ] && source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
-        [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ] && source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-        [ -f /etc/zshenv ] && source /etc/zshenv
-        [ -d ${pkgs.fzf} ] && source ${pkgs.fzf}/share/fzf/completion.zsh && source ${pkgs.fzf}/share/fzf/key-bindings.zsh
-        if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-          eval "$(ssh-agent -s)"
-        fi
-      '';
-      initContent = lib.mkMerge [
-        (lib.mkAfter ''
-          fzf_tab_preview() {
-            if [[ -d $path ]]; then
-              ${pkgs.eza}/bin/eza --icons=always --color=always "$1"
-            else
-              ${pkgs.bat}/bin/bat -p --color=always "$1"
-            fi
-          }
 
-          if [ -f "$HOME/.config/bat/themes/Catppuccin\ Mocha.tmTheme" ]; then
-            mkdir -p "$HOME/.config/bat/themes"
-            wget -P "$HOME/.config/bat/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
+    programs = {
+      fzf = {
+        enable = true;
+        enableZshIntegration = true;
+      };
+      fastfetch = {
+        enable = true;
+      };
+      zoxide = {
+        enable = true;
+      };
+      direnv = {
+        enable = true;
+        silent = true;
+        nix-direnv = {
+          enable = true;
+        };
+      };
+      bat = {
+        enable = true;
+        config.theme = "Catppuccin Mocha";
+      };
+      zsh = {
+        enable = true;
+        completionInit = ''
+          autoload -U compinit
+          compinit
+        '';
+        autosuggestion = {
+          enable = true;
+        };
+        history = {
+          append = true;
+          expireDuplicatesFirst = false;
+          extended = false;
+          findNoDups = true;
+          ignoreAllDups = true;
+          ignoreDups = true;
+          ignoreSpace = true;
+          path = "${config.home.homeDirectory}/.zsh_history";
+          save = 5000;
+          share = true;
+          size = 5000;
+        };
+        enableCompletion = true;
+        autocd = true;
+        historySubstringSearch = {
+          enable = true;
+        };
+        shellAliases = {
+          "cls" = "clear";
+          "vim" = "nvim";
+          "nrs" = "sudo nixos-rebuild switch --flake /etc/nixos#$(hostname) |& nom";
+          "hms" = "home-manager switch --flake ~/.config/home-manager#$(hostname) --impure |& nom";
+          "ls" = "eza -h --icons=always";
+          "ll" = "ls -l";
+          "l" = "ls -l";
+          "la" = "ls -la";
+          "vg" =
+            "valgrind --leak-check=full --show-leak-kinds=all --trace-children=yes --track-fds=yes --track-origins=yes --show-reachable=yes";
+          "ssh" = "TERM=xterm-256color ssh";
+        };
+        zplug = {
+          enable = true;
+          zplugHome = "${config.xdg.dataHome}/.zplug";
+          plugins = [
+            { name = "Aloxaf/fzf-tab"; }
+            { name = "nix-community/nix-zsh-completions"; }
+            { name = "z-shell/F-Sy-H"; }
+            { name = "zsh-users/zsh-autosuggestions"; }
+            { name = "zsh-users/zsh-syntax-highlighting"; }
+          ];
+        };
+        envExtra = ''
+          [ -f "$HOME/.zvars"  ] && source "$HOME/.zvars";
+          [ -f "$HOME/.nix-profile/etc/profile.d/nix.sh" ]  && source "$HOME/.nix-profile/etc/profile.d/nix.sh"
+          [ -f "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ] && source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+          [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ] && source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+          [ -f /etc/zshenv ] && source /etc/zshenv
+          [ -d ${pkgs.fzf} ] && source ${pkgs.fzf}/share/fzf/completion.zsh && source ${pkgs.fzf}/share/fzf/key-bindings.zsh
+          if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+            eval "$(ssh-agent -s)"
           fi
-        '')
-        (lib.mkAfter ''
-          zplug load
-          zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
-          zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-          zstyle ':completion:*' menu no
+        '';
+        initContent = lib.mkMerge [
+          (lib.mkAfter ''
+            fzf_tab_preview() {
+              if [[ -d $path ]]; then
+                ${pkgs.eza}/bin/eza --icons=always --color=always "$1"
+              else
+                ${pkgs.bat}/bin/bat -p --color=always "$1"
+              fi
+            }
 
-          bindkey '^R' fzf-history-widget
-          bindkey '^[[1;5C' forward-word
-          bindkey '^[[1;5D' backward-word
-          bindkey '^[Oc' forward-word
-          bindkey '^[Od' backward-word
-          bindkey '^[[1;2D' beginning-of-line
-          bindkey '^[[1;2C' end-of-line
-          bindkey '^[[1;3D' beginning-of-line
-          bindkey '^[[1;3C' end-of-line
-          bindkey '^H' backward-kill-word
-          bindkey -r '^['
+            if [ -f "$HOME/.config/bat/themes/Catppuccin\ Mocha.tmTheme" ]; then
+              mkdir -p "$HOME/.config/bat/themes"
+              wget -P "$HOME/.config/bat/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
+            fi
+          '')
+          (lib.mkAfter ''
+            zplug load
+            zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+            zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+            zstyle ':completion:*' menu no
 
-          autoload -Uz zmv
-          autoload -U edit-command-line
-          zle -N edit-command-line
-          bindkey '^x^e' edit-command-line
-        '')
-      ];
-      loginExtra = ''
-        eval "$(${pkgs.direnv}/bin/direnv hook zsh)"
-        eval "$(${pkgs.starship}/bin/starship init zsh)"
-      '';
-      sessionVariables = {
-        PATH = "$HOME/.nix-profile/bin:$HOME/.local/bin:/Library/Developer/CommandLineTools/Library/PrivateFrameworks/LLDB.framework/Versions/A/Resources/debugserver:$PATH";
-        MANPAGER = "/bin/sh -c 'col -bx | bat -l man --style=plain --paging=always'";
-        MANROFFOPT = "-c";
-        EDITOR = "nvim";
-        FZF_CTRL_R_OPTS = "--layout reverse --bind='ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort' --header='Ctrl-Y pour copier'";
+            bindkey '^R' fzf-history-widget
+            bindkey '^[[1;5C' forward-word
+            bindkey '^[[1;5D' backward-word
+            bindkey '^[Oc' forward-word
+            bindkey '^[Od' backward-word
+            bindkey '^[[1;2D' beginning-of-line
+            bindkey '^[[1;2C' end-of-line
+            bindkey '^[[1;3D' beginning-of-line
+            bindkey '^[[1;3C' end-of-line
+            bindkey '^H' backward-kill-word
+            bindkey -r '^['
+
+            autoload -Uz zmv
+            autoload -U edit-command-line
+            zle -N edit-command-line
+            bindkey '^x^e' edit-command-line
+          '')
+        ];
+        loginExtra = ''
+          eval "$(${pkgs.direnv}/bin/direnv hook zsh)"
+          eval "$(${pkgs.starship}/bin/starship init zsh)"
+        '';
+        sessionVariables = {
+          PATH = "$HOME/.nix-profile/bin:$HOME/.local/bin:/Library/Developer/CommandLineTools/Library/PrivateFrameworks/LLDB.framework/Versions/A/Resources/debugserver:$PATH";
+          MANPAGER = "/bin/sh -c 'col -bx | bat -l man --style=plain --paging=always'";
+          MANROFFOPT = "-c";
+          EDITOR = "nvim";
+          FZF_CTRL_R_OPTS = "--layout reverse --bind='ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort' --header='Ctrl-Y pour copier'";
+        };
       };
     };
   };
